38b79ec72310bb8a9d155bddcca5f87610fa68b0
Igor Kalkov <ikalkov@users.noreply.github.com>
2016-03-16 12:39:58 +0100

    Use default jf config for kernel build

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,11 +34,11 @@ TARGET_CPU_VARIANT := krait
 TARGET_BOOTLOADER_BOARD_NAME := MSM8960
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 zcache msm_rtb.filter=0x3F ehci-hcd.park=3
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 acpi_irq_nobalance noirqbalance isolcpus=1 androidboot.hardware=qcom user_debug=31 zcache msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_KERNEL_BASE := 0x80200000
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_KERNEL_CONFIG := cyanogen_jf_defconfig
+TARGET_KERNEL_CONFIG := jf_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/jf
 
 # Audio
